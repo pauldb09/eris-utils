cdcbb54ff24e68a29c768e46cc0da29295af263f
Pauldb09 <pauldeboissieu@yahoo.com>
2022-07-13 19:08:02 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { collectorManager } from "./struct/collectors/manager";
 import { SweeperManager } from "./struct/sweepers/manager"
 import { extend } from "./struct/utils/extend";
-export { collectorManager, extend, SweeperManager}
+export { collectorManager, extend, SweeperManager }
--- src/struct/collectors/manager.ts
+++ src/struct/collectors/manager.ts
@@ -50,9 +50,10 @@ export class collectorManager extends EventEmitter<collectorManagerEvents> {
         let userId = context.member && context.member.id ? context.member.id : context.author && context.author.id ? context.author.id : context.user && context.user.id ? context.user.id : "ERROR";
         let type = context.version ? "INTERACTION" : "MESSAGE";
         if (userId === "ERROR") throw new Error("Could not resolve an user id for message");
-        const list = new ExtendedArray(this.collectors.filter(col => col.channelId === context.channel.id && col.type === type && !col.ended && col.userId ? col.userId === userId : null))
+        const list = new ExtendedArray(this.collectors.filter(col => col.channelId === context.channel.id && col.type === type && !col.ended))
         if (list && list.length) {
             for (const col of list) {
+                if(col.userId && col.userId !== userId) return
                 try {
                     col.filter(context);
                 } catch (error) {
